e3bda6f260b1b4907cbf8783709ac9c3747d771b
Tales Henn <taleshenn@gmail.com>
2024-08-09 10:27:28 -0300

    fix

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 function trocaTema() {
   const html = document.documentElement;
   html.classList.toggle("light");
-}
 
-// Salva a preferência no LocalStorage
-if (html.classList.contains("light")) {
-  localStorage.setItem("theme", "light");
-} else {
-  localStorage.setItem("theme", "");
+  // Salva a preferência no LocalStorage
+  if (html.classList.contains("light")) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "");
+  }
 }
 
 // Verifica a preferência ao carregar a página
