92583bc9851c150576112d5a73a285e22f02b267
Tales Henn <taleshenn@gmail.com>
2024-08-09 11:26:50 -0300

    atualizar variaveis

--- js/app.js
+++ js/app.js
@@ -1,29 +1,38 @@
+localStorage.clear();
+console.log(
+  "O sistema prefere o modo de cor:",
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "ESCURO"
+    : "CLARO",
+  "se o site está sendo mostrado no modo errado pode ser necessário limpar o LocalStorage usando o código comentado na primeira linha."
+);
+
 function trocaTema() {
   const html = document.documentElement;
   html.classList.toggle("light");
 
   // Salva a preferência no LocalStorage
   if (html.classList.contains("light")) {
-    localStorage.setItem("theme", "light");
+    localStorage.setItem("tema", "light");
   } else {
-    localStorage.setItem("theme", "");
+    localStorage.setItem("tema", "");
   }
 }
 
-// Verifica a preferência ao carregar a página
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme) {
-  document.documentElement.classList.add(savedTheme);
+// Ao carregar a página, verifica no LocalStorage se há preferência por tema
+const temaSalvo = localStorage.getItem("tema");
+if (temaSalvo) {
+  document.documentElement.classList.add(temaSalvo);
 } else {
   // Verifica a preferência do navegador
-  const prefersLight = window.matchMedia(
+  const prefereLight = window.matchMedia(
     "(prefers-color-scheme: light)"
   ).matches;
-  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const prefereDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-  if (prefersLight) {
+  if (prefereLight) {
     document.documentElement.classList.add("light");
-  } else if (prefersDark) {
+  } else if (prefereDark) {
     document.documentElement.classList.remove("light");
   }
 }
